9a0798747c8f2d790c089856ad7ea3a44819bb4f
jsntrgsy <52066783+jsntrgsy@users.noreply.github.com>
2022-02-19 23:33:44 +0800

    add

---
+++ trunk/configs/boards/NEWIFI-D1/board.h
@@ -0,0 +1,23 @@
+/* Lenovo NEWIFI-D1 */
+
+#define BOARD_PID		"NEWIFI-D1"
+#define BOARD_NAME		"NEWIFI-D1"
+#define BOARD_DESC		"Lenovo NEWIFI-D1 Wireless Router"
+#define BOARD_VENDOR_NAME	"Lenovo(Beijing) Limited"
+#define BOARD_VENDOR_URL	"http://www.newifi.com/"
+#define BOARD_MODEL_URL		"http://www.newifi.com/product_newifi2_summary.shtml"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
+
